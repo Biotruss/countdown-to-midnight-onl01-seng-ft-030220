c63795339a043028e79562b8f73a752543181383
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-06 00:29:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,8 +11,8 @@ end
 
 def countdown_with_sleep(integer)
   countdown_with_sleep = 5
-  while countdown_with_sleep < 5
+  while countdown_with_sleep != 0
     sleep(1)
-    countdown_with_sleep += 1 
+    countdown_with_sleep -= 1 
   end
 end
