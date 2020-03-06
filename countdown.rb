0326cba7ac670dcd545d02d5ccfb9117162d0756
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-06 00:19:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(integer)
-  countdown_with_sleep = 0
-  while countdown_with_sleep < 10
-    countdown_with_sleep += 1 
+  countdown_with_sleep = 10
+  while countdown_with_sleep > 0
+    countdown_with_sleep -= 1 
     puts "#{countdown_with_sleep} SECOND(S)!"
   end
 end
