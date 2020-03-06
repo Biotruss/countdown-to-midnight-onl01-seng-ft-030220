9eb5322bfffbd3a5bdce0d43c79a997440a0f955
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-06 00:20:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(integer)
   countdown_with_sleep = 10
   while countdown_with_sleep > 0
-    countdown_with_sleep -= 1 
     puts "#{countdown_with_sleep} SECOND(S)!"
+    countdown_with_sleep -= 1
   end
 end
